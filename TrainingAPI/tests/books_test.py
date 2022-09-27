74bd9c661320c4ab1b8ca7e94ba11b840d2cf7dc
Thao Nguyen <thaophuong.tracy@gmail.com>
2022-09-27 07:18:42 +0700

    task6: have only written 3 tests

--- TrainingAPI/tests/books_test.py
+++ TrainingAPI/tests/books_test.py
@@ -14,6 +14,29 @@ class BooksTests(unittest.TestCase):
         self.assertIsInstance(data.get('books'), list)
 
     # TODO: unittest for another apis
+    # def test_post_book_by_id(self):
+    #     request, response = app.test_client.post('/books')
+    #     self.assertEqual(response.status, 200)
+    #     data = json.loads(response.text)
+    #     self.assertEqual(data.get('status'), 'success')
+
+    def test_get_book_by_id(self):
+        request, response = app.test_client.get('/books/2ffa5b23-b279-45d1-b7ca-721209c216b7')
+        self.assertEqual(response.status, 200)
+        data = json.loads(response.text)
+        self.assertIsInstance(data.get('book'), object)
+
+    # def test_put_book_by_id(self):
+    #     request, response = app.test_client.put('/books/2ffa5b23-b279-45d1-b7ca-721209c216b7')
+    #     self.assertEqual(response.status, 200)
+    #     data = json.loads(response.text)
+    #     self.assertEqual(data.get('status'), 'success')
+
+    def test_delete_book_by_id(self):
+        request, response = app.test_client.delete('/books/2ffa5b23-b279-45d1-b7ca-721209c216b7')
+        self.assertEqual(response.status, 200)
+        data = json.loads(response.text)
+        self.assertEqual(data.get('status'), 'success')
 
 
 if __name__ == '__main__':
